9a6f3b95ddac1f19abcab2d6cca335b6ae8e5563
tom-curphey <mail@tomcurphey.com>
2019-02-25 07:29:16 +1100

    Updates

---
+++ precache-manifest.59df4770d5ce24247755e5eaf3c53a8c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/test-repo/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "5d4e4a9d90375aadd97f",
+    "url": "/test-repo/static/js/runtime~main.5d4e4a9d.js"
+  },
+  {
+    "revision": "6bb4a2e5fab1ba1c0d16",
+    "url": "/test-repo/static/js/main.6bb4a2e5.chunk.js"
+  },
+  {
+    "revision": "5bf9b2cda7c63b962686",
+    "url": "/test-repo/static/js/2.5bf9b2cd.chunk.js"
+  },
+  {
+    "revision": "6bb4a2e5fab1ba1c0d16",
+    "url": "/test-repo/static/css/main.08323fa4.chunk.css"
+  },
+  {
+    "revision": "c28699c03deaf07f7c620938be80db39",
+    "url": "/test-repo/index.html"
+  }
+];
